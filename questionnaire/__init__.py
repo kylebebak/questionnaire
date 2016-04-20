1acdd364e6f26787e82e07c121d01b864e86b913
Kyle Bebak <kylebebak@gmail.com>
2016-04-20 17:02:49 -0500

    removed methods that did nothing but return values from instances

--- questionnaire/__init__.py
+++ questionnaire/__init__.py
@@ -8,8 +8,8 @@ from pick import Picker
 
 
 def back_pick(_options, prompt="Pick: ", indicator='=>'):
-    """Instantiates a picker, registers custom handlers for
-    going back, and starts the picker.
+    """Instantiates a picker, registers custom handlers for going back,
+    and starts the picker.
     """
     def go_back(picker):
         return None, -1
@@ -20,8 +20,8 @@ def back_pick(_options, prompt="Pick: ", indicator='=>'):
     return picker.start()
 
 
-def back_pick_multiple(_options, prompt="Pick multiple: ",
-                       indicator='=>', ALL="all", DONE="done..."):
+def back_pick_multiple(_options, prompt="Pick multiple: ", indicator='=>',
+                       ALL="all", DONE="done..."):
     """Calls `pick` in a while loop to allow user to pick multiple
     options. Returns a list of chosen options.
     """
@@ -42,20 +42,20 @@ def back_pick_multiple(_options, prompt="Pick multiple: ",
 class Condition:
     """Container for condition properties.
     """
-    _OPERATORS = {
-        '==' : operator.eq,
-        '!=' : operator.ne,
-        '<=' : operator.le,
-        '>=' : operator.ge,
-        'in' : lambda x, y: x in y,
+    OPERATORS = {
+        '=='     : operator.eq,
+        '!='     : operator.ne,
+        '<='     : operator.le,
+        '>='     : operator.ge,
+        'in'     : lambda x, y: x in y,
         'not in' : lambda x, y: x not in y
         }
 
     def get_operator(op):
-        return Condition._OPERATORS[op] if op in Condition._OPERATORS else op
+        return Condition.OPERATORS[op] if op in Condition.OPERATORS else op
 
     def check_operator(op):
-        if op in Condition._OPERATORS:
+        if op in Condition.OPERATORS:
             return True
         try:
             n_args = len(inspect.getargspec(op)[0])
@@ -64,52 +64,33 @@ class Condition:
             return False
 
     def __init__(self, condition):
-        self._keys = condition['keys']
-        self._vals = condition['vals']
-        self._operators = condition['operators'] \
-                          if 'operators' in condition \
-                          else ['==']*len(self._keys)
-        vars = [self._keys, self._vals, self._operators]
+        self.keys = condition['keys']
+        self.vals = condition['vals']
+        if 'operators' in condition:
+            self.operators = condition['operators']
+        else:
+            self.operators = ['==']*len(self.keys)
 
+        vars = [self.keys, self.vals, self.operators]
         assert all(type(var) == list for var in vars), \
             "All condition properties must be lists"
         assert all(len(var) == len(vars[0]) for var in vars), \
             "All condition properties must have the same length"
-        assert all(Condition.check_operator(op) for op in self._operators), \
+        assert all(Condition.check_operator(op) for op in self.operators), \
             "Condition has invalid operator(s)"
-
-    def keys(self):
-        return self._keys
-
-    def vals(self):
-        return self._vals
-
-    def operators(self):
-        return self._operators
 
 
 class Question:
     """Container for question properties.
     """
     def __init__(self, key, options, multiple=False, condition={}):
-        self._key = key
-        self._options = options
-        self._multiple = multiple
-        self._condition = Condition(condition) \
-                          if 'keys' in condition and 'vals' in condition \
-                          else None
-
-    def key(self):
-        return self._key
-
-    def options(self):
-        return self._options
-
-    def multiple(self):
-        return self._multiple
-
-    def condition(self):
-        return self._condition
+        self.key = key
+        self.options = options
+        self.multiple = multiple
+        if 'keys' in condition and 'vals' in condition:
+            self.condition = Condition(condition)
+        else:
+            self.condition = None
 
 
 class Questionnaire:
@@ -156,7 +137,7 @@ class Questionnaire:
         """Add a Question instance to the questions dict. Each key points
         to a list of Question instances with that key.
         """
-        self.questions.setdefault(question.key(), []).append(question)
+        self.questions.setdefault(question.key, []).append(question)
 
     def which_question(self, key):
         """Decide which Question instance to select from the list of questions
@@ -165,7 +146,7 @@ class Questionnaire:
         """
         questions = self.questions[key]
         for question in questions:
-            if self.check_condition(question.condition()):
+            if self.check_condition(question.condition):
                 return question
         return None
 
@@ -175,7 +156,7 @@ class Questionnaire:
         if not condition:
             return True
         for key, val, op in \
-            zip(condition.keys(), condition.vals(), condition.operators()):
+            zip(condition.keys, condition.vals, condition.operators):
             if not Condition.get_operator(op)(val, self.choices[key]):
                 return False
         return True
@@ -197,7 +178,7 @@ class Questionnaire:
         for key in self.questions.keys():
             question = self.which_question(key)
             if question is not None and key not in self.choices:
-                if not self.prompt(key, question.options(),
-                        multiple=question.multiple()):
+                if not self.prompt(key, question.options,
+                        multiple=question.multiple):
                     return False
         return True
