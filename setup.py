196524449ff5f63841be673f36d70c76d894dd4a
Kyle Bebak <kylebebak@gmail.com>
2016-04-20 22:29:49 -0500

    included tags license

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ def fread(fname):
 
 setup(
     name='questionnaire',
-    version='0.4.1',
+    version='0.4.2',
     description='Uses https://github.com/wong2/pick to prompt user to fill' \
                 ' out a questionnaire, and returns the results as a dict',
     long_description=fread('README.md'),
     keywords='terminal gui pick',
     url='https://github.com/kylebebak/questionnaire',
-    download_url = 'https://github.com/kylebebak/questionnaire/tarball/0.4.1',
+    download_url = 'https://github.com/kylebebak/questionnaire/tarball/0.4.2',
     author='kylebebak',
     author_email='kylebebak@gmail.com',
     license='MIT',
