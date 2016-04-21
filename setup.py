1ccc1652d5e58723cc9551cbf129a210bcdc43d2
Kyle Bebak <kylebebak@gmail.com>
2016-04-20 22:21:48 -0500

    included function in setup.py for including long_description, based on readme

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
+import os
 from setuptools import setup
+
+def fread(fname):
+    filepath = os.path.join(os.path.dirname(__file__), fname)
+    with open(filepath, 'r') as fp:
+        return fp.read()
 
 setup(
     name='questionnaire',
-    version='0.3.0',
+    version='0.4.0',
     description='Uses https://github.com/wong2/pick to prompt user to fill' \
                 ' out a questionnaire, and returns the results as a dict',
+    long_description=fread('README.md'),
     keywords='terminal gui pick',
     url='https://github.com/kylebebak/questionnaire',
-    download_url = 'https://github.com/kylebebak/questionnaire/tarball/0.3.0',
+    download_url = 'https://github.com/kylebebak/questionnaire/tarball/0.4.0',
     author='kylebebak',
     author_email='kylebebak@gmail.com',
     license='MIT',
