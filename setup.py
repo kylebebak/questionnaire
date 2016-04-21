bf99d3e48f486c44087ca7ffc562c0cd61581a87
Kyle Bebak <kylebebak@gmail.com>
2016-04-20 22:28:34 -0500

    included readme and license in manifest.in

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ def fread(fname):
 
 setup(
     name='questionnaire',
-    version='0.4.0',
+    version='0.4.1',
     description='Uses https://github.com/wong2/pick to prompt user to fill' \
                 ' out a questionnaire, and returns the results as a dict',
     long_description=fread('README.md'),
     keywords='terminal gui pick',
     url='https://github.com/kylebebak/questionnaire',
-    download_url = 'https://github.com/kylebebak/questionnaire/tarball/0.4.0',
+    download_url = 'https://github.com/kylebebak/questionnaire/tarball/0.4.1',
     author='kylebebak',
     author_email='kylebebak@gmail.com',
     license='MIT',
