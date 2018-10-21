ed92642e8a2a0198da198acbcde2707f1d528585
Kyle Bebak <kylebebak@gmail.com>
2018-10-20 19:21:40 -0500

    Prepares for 2.2.0 release

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from setuptools import setup
 
 setup(
     name='questionnaire',
-    version='2.1.0',
+    version='2.2.0',
     description='Elegant mini-DSL for creating command line questionnaires',
     long_description='Check it out on GitHub...',
     keywords='terminal gui pick question option',
     url='https://github.com/kylebebak/questionnaire',
-    download_url='https://github.com/kylebebak/questionnaire/tarball/2.1.0',
+    download_url='https://github.com/kylebebak/questionnaire/tarball/2.2.0',
     author='kylebebak',
     author_email='kylebebak@gmail.com',
     license='MIT',
     packages=['questionnaire'],
-    install_requires=['pick==0.6.0'],
+    install_requires=['pick==0.6.4'],
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
